8cf6e1fb2078a42f71516d3adc39fe96e4e5ff1b
Davi Lessa <davilessa2002@gmail.com>
2022-05-09 00:51:37 -0300

    exercicio6.js

---
+++ Projetos/exercicio6.js
@@ -0,0 +1,14 @@
+function inverso(x) {
+    if(typeof x == "boolean")
+        return !x 
+    else if(typeof x == "number")
+        return -x
+    else
+        return `booleano ou número esperados, mas o parâmetro é do tipo ${typeof x}`
+}
+
+
+console.log(inverso(true))
+console.log(inverso("6"))
+console.log(inverso(-2000))
+console.log(inverso("Javascript"))
