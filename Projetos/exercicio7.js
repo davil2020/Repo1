c1966b5c591bcf687023804e93a253b1a4859f98
Davi Lessa <davilessa2002@gmail.com>
2022-05-09 00:51:51 -0300

    exercicio7.js

---
+++ Projetos/exercicio7.js
@@ -0,0 +1,10 @@
+function estaEntre(numero, maximo, minimo, inclusivo = false) {
+    if(inclusivo)
+        return numero <= maximo && numero >= minimo
+    
+    return numero < maximo && numero > minimo
+}
+
+console.log(estaEntre(10,100,50))
+console.log(estaEntre(16,100,160))
+console.log(estaEntre(3, 150, 3, true))
